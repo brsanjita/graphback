b7d9b505346c752b9b04d32d42d36a22a8e9cb0d
Enda Phelan <ephelan@redhat.com>
2020-02-12 13:19:37 +0000

    fix: import alias

--- packages/graphback-runtime/src/service/CRUDService.ts
+++ packages/graphback-runtime/src/service/CRUDService.ts
@@ -1,5 +1,5 @@
 import { GraphbackOperationType, ModelTableMap, upperCaseFirstChar } from "@graphback/core"
-import DataLoader from "dataloader";
+import * as DataLoader from "dataloader";
 import { GraphQLObjectType } from 'graphql';
 import { PubSubEngine } from 'graphql-subscriptions';
 import { GraphbackDataProvider } from "../data/GraphbackDataProvider";
